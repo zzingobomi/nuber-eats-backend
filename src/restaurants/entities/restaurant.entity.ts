3f957400b21719c2a73d79816646ceb69a37c198
ohjongchan <jongchan_oh@tmax.co.kr>
2022-08-09 17:06:43 +0900

    TYPEORM AND NEST

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,16 +1,29 @@
-import { Field, ObjectType } from '@nestjs/graphql';
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import { IsBoolean, IsOptional, IsString, Length } from 'class-validator';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@InputType({ isAbstract: true })
 @ObjectType()
+@Entity()
 export class Restaurant {
+  @PrimaryGeneratedColumn()
+  @Field(type => Number)
+  id: number;
+
   @Field(type => String)
+  @Column()
+  @IsString()
+  @Length(5)
   name: string;
 
-  @Field(type => Boolean)
+  @Field(type => Boolean, { nullable: true })
+  @Column({ default: true })
+  @IsOptional()
+  @IsBoolean()
   isVegan: boolean;
 
-  @Field(type => String)
+  @Field(type => String, { defaultValue: '강남' })
+  @IsString()
+  @Column()
   address: string;
-
-  @Field(type => String)
-  ownersName: string;
 }
