aa8fa4a69d9419dc44b48d2ea6dd7e8976c1ca80
ohjongchan <jongchan_oh@tmax.co.kr>
2022-08-09 17:55:47 +0900

    USER CRUD Part 4.3

---
+++ src/users/users.resolver.ts
@@ -0,0 +1,16 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { User } from './entities/user.entity';
+import { UsersService } from './users.service';
+
+@Resolver(of => User)
+export class UsersResolver {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Query(returns => Boolean)
+  hi() {
+    return true;
+  }
+
+  //@Mutation(returns => Boolean)
+  //createAccount(@Args("input") createAccountInput: )
+}
