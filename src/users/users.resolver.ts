cd49cafe70af83fd1d8c55f43dca2094ccc77a68
ohjongchan <jongchan_oh@tmax.co.kr>
2022-08-10 16:44:39 +0900

    USER CRUD Part.2

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,4 +1,9 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import {
+  CreateAccountInput,
+  CreateAccountOutput,
+} from './dtos/create-account.dto';
+import { LoginInput, LoginOutput } from './dtos/login.dto';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 
@@ -11,6 +16,29 @@ export class UsersResolver {
     return true;
   }
 
-  //@Mutation(returns => Boolean)
-  //createAccount(@Args("input") createAccountInput: )
+  @Mutation(returns => CreateAccountOutput)
+  async createAccount(
+    @Args('input') createAccountInput: CreateAccountInput,
+  ): Promise<CreateAccountOutput> {
+    try {
+      return this.usersService.createAccount(createAccountInput);
+    } catch (error) {
+      return {
+        ok: false,
+        error,
+      };
+    }
+  }
+
+  @Mutation(returns => LoginOutput)
+  async login(@Args('input') loginInput: LoginInput): Promise<LoginOutput> {
+    try {
+      return this.usersService.login(loginInput);
+    } catch (error) {
+      return {
+        ok: false,
+        error,
+      };
+    }
+  }
 }
